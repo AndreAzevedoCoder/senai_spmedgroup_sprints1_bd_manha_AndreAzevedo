cd2b3410ec9bc8cec059f4a748bdeb8d306097c3
André Santos De Azevedo <54094513841@info.local>
2020-02-11 11:23:58 -0200

    Segundo dia de projeto fix

--- Scripts/SPMedGroup_DQL.sql
+++ Scripts/SPMedGroup_DQL.sql
@@ -1,2 +1,11 @@
 USE SPMedGroup_Manha
 
+SELECT * FROM consulta
+INNER JOIN filial ON filial.idFilial = consulta.idFilial
+INNER JOIN medicos ON medicos.idMedico = consulta.idMedicos
+INNER JOIN especialidade ON especialidade.idEspecialidade = medicos.idEspecialidade
+
+SELECT situacao,dataDaConsulta,endereco,nome,endereco,tituloEspecialidade FROM consulta
+INNER JOIN filial ON filial.idFilial = consulta.idFilial
+INNER JOIN medicos ON medicos.idMedico = consulta.idMedicos
+INNER JOIN especialidade ON especialidade.idEspecialidade = medicos.idEspecialidade
